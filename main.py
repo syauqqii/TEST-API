d7379142adc596fb897016bf58c654a4fb9fa9ac
Qii <57295427+syauqqii@users.noreply.github.com>
2023-03-29 20:01:07 +0700

    Update main.py
    Seperti biasa, meningkatkan performa dan meminimalisir akan terjadinya bug pada program ini

--- main.py
+++ main.py
@@ -1,45 +1,82 @@
 from os import system, path, name
-import json
-import re
+from re import compile, match, IGNORECASE
+from json import dumps, loads
+
+if name == "posix":
+    try:
+        from getch import getch
+    except ModuleNotFoundError:
+        system("pip install getch")
+        try:
+            from getch import getch
+        except ImportError:
+            exit(0)
+elif name == "nt":
+    try:
+        from msvcrt import getch
+    except ModuleNotFoundError:
+        system("pip install msvcrt")
+        try:
+            from msvcrt import getch
+        except ImportError:
+            exit(0)
+
+def clearScreen():
+    if name == "nt":
+        system("cls")
+    elif name == "posix":
+        system("clear")
+    else:
+        return
 
 # Menambahkan validasi input URL
-def is_url(string):
-    regex = re.compile(
+def isURL(string):
+    regex = compile(
         r'^(?:http|ftp)s?://' # http:// atau https://
         r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' # domain
         r'localhost|' # localhost
         r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ip
         r'(?::\d+)?' # opsional: port
-        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
-    return re.match(regex, string) is not None
+        r'(?:/?|[/?]\S+)$', IGNORECASE)
+    return match(regex, string) is not None
+
+def falseURL():
+    print("\n  {")
+    print("    \"status\" : \"error\",")
+    print("    \"message\": \"yang anda inputkan bukanlah URL yang valid!\"")
+    print("  }")
+    print("\n\n [!] tekan apa saja untuk keluar!", end='')
+    input("")
+    exit(0)
+
+def readFileName(filename):
+    if name == "nt":
+        system(f"type {filename}")
+    elif name == "posix":
+        system(f"cat {filename}")
+    else:
+        return
+
+def banner():
+   # Design: https://patorjk.com/software/taag/#p=display&f=Big%20Money-nw&t=
+    print("""\n     $$$$$$$$\\ $$$$$$$$\\  $$$$$$\\ $$$$$$$$\\       $$$$$$\\  $$$$$$$\\ $$$$$$\\
+     \\__$$  __|$$  _____|$$  __$$\\\\__$$  __|     $$  __$$\ $$  __$$\\\\_$$  _|
+        $$ |   $$ |      $$ /  \__|  $$ |        $$ /  $$ |$$ |  $$ | $$ |
+        $$ |   $$$$$\    \\$$$$$$\\    $$ |$$$$$$\\ $$$$$$$$ |$$$$$$$  | $$ |
+        $$ |   $$  __|    \\____$$\   $$ |\______|$$  __$$ |$$  ____/  $$ |
+        $$ |   $$ |      $$\\   $$ |  $$ |        $$ |  $$ |$$ |       $$ |
+        $$ |   $$$$$$$$\\ \\$$$$$$  |  $$ |        $$ |  $$ |$$ |     $$$$$$\\
+        \\__|   \\________| \\______/   \\__|        \\__|  \\__|\\__|     \\______|
+    \n                    [ IG: @syaauqqii | GP2D-API CLI ]""")
 
 def main():
-    system("title Mini-project: TEST-API @syaauqqii" if name=='nt' else "")
-    system("cls" if name=='nt' else "clear")
-
-    # Design: https://patorjk.com/software/taag/#p=display&f=Big%20Money-nw&t=
-    print("\n $$$$$$$$\\ $$$$$$$$\\  $$$$$$\\ $$$$$$$$\\       $$$$$$\\  $$$$$$$\\ $$$$$$\\ ")
-    print(" \\__$$  __|$$  _____|$$  __$$\\\\__$$  __|     $$  __$$\ $$  __$$\\\\_$$  _|")
-    print("    $$ |   $$ |      $$ /  \__|  $$ |        $$ /  $$ |$$ |  $$ | $$ |  ")
-    print("    $$ |   $$$$$\    \\$$$$$$\\    $$ |$$$$$$\\ $$$$$$$$ |$$$$$$$  | $$ |  ")
-    print("    $$ |   $$  __|    \\____$$\   $$ |\______|$$  __$$ |$$  ____/  $$ |  ")
-    print("    $$ |   $$ |      $$\\   $$ |  $$ |        $$ |  $$ |$$ |       $$ |  ")
-    print("    $$ |   $$$$$$$$\\ \\$$$$$$  |  $$ |        $$ |  $$ |$$ |     $$$$$$\ ")
-    print("    \\__|   \\________| \\______/   \\__|        \\__|  \\__|\\__|     \\______|")
-    print("\n                    [ IG: @syaauqqii | GP2D-API CLI ]                           ")
-
+    filename = "result.json"
+    clearScreen()
+    banner()
     print("\n     [#] FORMAT INPUT url:\n         > ex: https://reqres.in/api/users")
     url  = str(input("\n [>] INPUT url : "))
-    if is_url(url):
-        pass
-    else:
-        print("\n  {")
-        print("    \"status\" : \"error\",")
-        print("    \"message\": \"yang anda inputkan bukanlah URL yang valid!\"")
-        print("  }")
-        print("\n [!] tekan ENTER untuk keluar! [ENTER]", end='')
-        input("")
-        exit(0)
+    if isURL(url): pass
+    else: falseURL()
     print("\n     [#] HTTP:\n         [1] GET    [3] PUT\n         [2] POST   [4] DELETE\n")
     http = int(input(" [>] INPUT http: (1..4) "))
     print()
@@ -56,8 +93,8 @@ def main():
     if http in [2, 3]:
         print("     [#] FORMAT INPUT data:\n         > ex: name=dimas&job=tidur\n")
         data = input(" [>] INPUT data: ")
-        data_json = json.dumps(dict([tuple(d.split('=')) for d in data.split('&')]))
-        data_query = '&'.join([f"{k}={v}" for k, v in json.loads(data_json).items()])
+        data_json = dumps(dict([tuple(d.split('=')) for d in data.split('&')]))
+        data_query = '&'.join([f"{k}={v}" for k, v in loads(data_json).items()])
         print()
         cmd = f'curl -s -X {req} -d "{data_query}" {url} | jq > result.json'
         system(cmd)
@@ -66,38 +103,35 @@ def main():
 
     print(" [#] RESULT:\n")
 
-    if path.isfile("result.json"):
-        with open("result.json", "r") as f:
-            txt = ' ' + f.read().replace('\n', '\n ')
+    if path.isfile(filename):
+        with open(filename, "r") as f:
+            lines = f.readlines()
 
-        try:
-            data = json.loads(txt)
-        except json.JSONDecodeError as e:
-            print(f"Invalid JSON format in result.json: {e}")
-            return
+        with open(filename, "w") as f:
+            for line in lines:
+                f.write('  ' + line)
 
-        with open("result.json", "w") as f:
-            json_string = json.dumps(data, indent=2)
-            spaced_json_string = json_string.replace('\n', '\n     ')
-            f.write(spaced_json_string)
-
-        system("cat result.json || type result.json")
+        readFileName(filename)
 
     else:
         print("Result file is missing")
 
-    print("\n\n [!] ENTER untuk keluar!", end='')
-    input("")
-
 if __name__ == "__main__":
     try:
-        main()
+        system("title Mini-project: TEST-API @syaauqqii" if name=='nt' else "")
+        while True:
+             main()
+             print("\n\n [!] tekan [ENTER] / [ESC] untuk keluar!", end='')
+             if getch() in [b'\r', b'\x1b']: print(); break 
     except KeyboardInterrupt:
-        # system("cls" if name=='nt' else "clear")
         print("\n\n  {")
         print("    \"status\" : \"success\",")
         print("    \"message\": \"berhasil exit dengan CTRL + C\"")
         print("  }")
-        print("\n [!] tekan ENTER untuk keluar! [ENTER]", end='')
-        input("")
+        print("\n\n [!] tekan apa saja untuk keluar!", end='')
+        try:
+            input("")
+        except KeyboardInterrupt:
+            print()
+            exit(0)
         exit(0)
